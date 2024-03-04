add7e0455422e7bbc808206d64ad7dd35d9c3567
ZinZinThan <zinzinn1507@gmail.com>
2024-03-04 13:11:34 +0630

    Add project files.

---
+++ ConsoleApp/Convert/AlienSymbolToInteger.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Convert
+{
+    public class AlienSymbolToInteger
+    {
+        static Dictionary<char, int> symbol = new Dictionary<char, int>
+        {
+            {'A', 1},
+            {'B', 5},
+            {'Z', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'R', 1000}
+        };
+
+        public void Main()
+        {
+            while (true)
+            {
+                Console.Write("Enter your input that contains only ABZLCDR characters: ");
+                string userInput = Console.ReadLine()!;
+
+                if (CheckCharacter(userInput, "ABZLCDR"))
+                {
+                    Console.WriteLine("Input contains unspecific characters.");
+                }
+                else
+                {
+                    Console.WriteLine("Integer of " + userInput + " is => " + ConvertToInteger(userInput));
+                }
+            }
+        }
+
+        static bool CheckCharacter(string input, string allowedCharacters)
+        {
+            string pattern = $"[^{Regex.Escape(allowedCharacters)}]";
+            return Regex.IsMatch(input, pattern);
+        }
+
+        public static int ConvertToInteger(string s)
+        {
+            int result = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i < s.Length - 1 && symbol[s[i]] < symbol[s[i + 1]])
+                {
+                    result += symbol[s[i + 1]] - symbol[s[i]];
+                    i++;
+                }
+                else
+                {
+                    result += symbol[s[i]];
+                }
+            }
+
+            return result;
+        }
+
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp.Convert;
+
+Console.WriteLine("Alien numerals are usually written from largest to smallest. However, the numeral for four is not AAAA. Instead, 4 is written as AB. Because A (1) is before B (5) we subtract it making four. The same principle applies to 9, which is written as A (1) and Z (10).");
+Console.WriteLine(" A =>1 ,\n B =>5 ,\n Z =>10 ,\n L =>50 ,\n C =>100 ,\n D =>500 ,\n R =>1000");
+
+AlienSymbolToInteger convert = new AlienSymbolToInteger();
+convert.Main();
